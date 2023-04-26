52e135a95b01f39f62425ed2105aac4edb2c7cf8
mord66 <sercangunbatti@gmail.com>
2023-04-26 20:55:56 +0300

    Complete game function

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ function getComputerChoice() {
     let choice = items[Math.floor(Math.random()*items.length)];
     return choice;
 }
-let compScore;
-let playerScore;
+let compScore = 0;
+let playerScore = 0;
 
 function playRound (compChoice, playerChoice){
 
@@ -15,46 +15,38 @@ function playRound (compChoice, playerChoice){
     compChoice = getComputerChoice();
     console.log("Player chose: " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1));
     console.log("Computer chose: " + compChoice.charAt(0).toUpperCase() + compChoice.slice(1));
-    
-    if (playerChoice == "paper") {
-        if (compChoice == "scissors") {
-            console.log("Computer won the round!");
-            compScore = compScore +1;
-        }
-        else if (compChoice == "rock"){
-            console.log("Player won the round!");
-            playerScore = playerScore + 1;
-        }
-        else {
-            console.log("It's a tie!");
-        }
-        }
-    else if (playerChoice == "rock") {
-        if (compChoice == "scissors"){
-            console.log("Player won the round!");
-            playerScore = playerScore +1;
-        }
-        else if (compChoice == "paper"){
-            console.log("Computer won the round!");
-            compScore = compScore +1;
-        }
-        else {
-            console.log("It's a tie!");
-        }
-        }
-    else if (playerChoice == "scissors"){
-        if (compChoice == "rock"){
-            console.log("Computer won the round!");
-            compScore = compScore +1;
-        }
-        else if (compChoice == "paper"){
-            console.log("Player won the round!");
-            playerScore = playerScore +1;
-        }
-        else {
-            console.log("It's a tie!");
-        }
+
+    if (playerChoice == compChoice){
+        console.log("It's a tie!")
+    }
+    else if (
+    (compChoice == "paper" && playerChoice == "rock")||
+    (compChoice == "rock" && playerChoice == "scissors")||
+    (compChoice == "scissors" && playerChoice == "paper")
+    ) {
+        console.log("Computer won the round!");
+        compScore = compScore +1;
+    }
+    else {
+        console.log("Player won the round!");
+        playerScore = playerScore + 1;
+
     }
     }
 
-playRound();
+function game() {
+    while (playerScore < 5 && compScore < 5) {
+        playRound();
+        console.log(playerScore);
+        console.log(compScore);
+    }
+    if (compScore == 5) {
+        console.log("Computer won the game!");
+    }
+    else if (playerScore == 5){
+        console.log("Player won the game!");
+    }
+}
+
+
+game();
