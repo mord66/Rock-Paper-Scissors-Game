b73f2fadf9b8036399256d72f1cac31cc948495c
mord66 <sercangunbatti@gmail.com>
2023-05-03 23:21:19 +0300

    Add UI

--- scripts/main.js
+++ scripts/main.js
@@ -3,50 +3,53 @@ function getComputerChoice() {
     let choice = items[Math.floor(Math.random()*items.length)];
     return choice;
 }
+
 let compScore = 0;
 let playerScore = 0;
 
 function playRound (compChoice, playerChoice){
 
-    playerChoice = (prompt("Rock, paper or scissors?")).toLowerCase();
+    /*playerChoice = (prompt("Rock, paper or scissors?")).toLowerCase();
     while (items.includes(playerChoice) == false){
-        playerChoice = (prompt("Invalid choice!\nRock, paper or scissors?")).toLowerCase();
-    }
+        playerChoice = (prompt("Invalid choice!\nRock, paper or scissors?")).toLowerCase(); 
+    }*/
     compChoice = getComputerChoice();
     console.log("Player chose: " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1));
     console.log("Computer chose: " + compChoice.charAt(0).toUpperCase() + compChoice.slice(1));
 
     if (playerChoice == compChoice){
-        console.log("It's a tie!")
+        announceBar.textContent="It's a tie!";
     }
     else if (
     (compChoice == "paper" && playerChoice == "rock")||
     (compChoice == "rock" && playerChoice == "scissors")||
     (compChoice == "scissors" && playerChoice == "paper")
     ) {
-        console.log("Computer won the round!");
+        announceBar.textContent="Computer won the round!";
         compScore = compScore +1;
     }
     else {
-        console.log("Player won the round!");
+        announceBar.textContent="Player won the round!";
         playerScore = playerScore + 1;
 
     }
     }
 
 function game() {
-    while (playerScore < 5 && compScore < 5) {
         playRound();
         console.log(playerScore);
         console.log(compScore);
-    }
-    if (compScore == 5) {
-        console.log("Computer won the game!");
-    }
-    else if (playerScore == 5){
-        console.log("Player won the game!");
-    }
 }
 
+const announceBar = document.querySelector(".announceBar");
+announceBar.textContent=""
 
-game();
+
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const choiceButtons = document.getElementById("choice-buttons");
+
+choiceButtons.addEventListener("click", function(e){
+    playRound(getComputerChoice(), (e.target.id));
+});
